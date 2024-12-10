6502aa96f50d6d0fb68a9b7f284bc7b6e86ef394
NikRedFox <nikolasbs11@gmail.com>
2024-12-10 17:09:42 -0300

    added javascript fundamentals

---
+++ src/js/fundamentals.js
@@ -0,0 +1,25 @@
+//variables
+let variavel = "alteravel"
+const constante = "imutavel"
+
+variavel = 1
+// Proibido reassign uma constante
+// constante = 2
+
+// console
+console.log(variavel)
+console.error(constante)
+console.info("informação")
+console.warn("var é depreciado")
+
+//tipos de dados
+const lastName = "correa"
+let age = 25
+const person = {
+    name: "João",
+    agr: 16,
+    gender: 'male'
+}
+const fruits = ["banana", 'uva']
+
+console.table(fruits)
